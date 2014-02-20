a432675084b493f753345cc346905271e3751486
jlgoldb2 <jlgoldb2@asu.edu>
2014-02-19 17:10:57 -0700

    delete neural_net.rb, refactored neuron.rb

--- lib/neuron/neuron.rb
+++ lib/neuron/neuron.rb
@@ -19,11 +19,18 @@ class Neuron
 		@neuron[:delay] = delay
 		print @neuron[:address], " is generated at ", @neuron[:location], " with delay of ", @neuron[:delay], " turns.\n"
 
-		#grow 10 dendrites
+		#grow 10 dendrites during initialization
 		counter = 0
 		10.times do 
 			@dendrites[counter] = grow_dendrite()
+			print @dendrites[counter], " "
 		end
+
+		print "\n"
+
+
+	
+
 
 	end
 
@@ -72,27 +79,20 @@ class Neuron
 					  # and return array of [x, y, z, distance]
 		
 		dendrite = []
-		@neuron[:location].each {|pt| [true,false].sample ? dendrite << pt+rand(10) : dendrite << pt-rand(10)}
-		dendrite << @neuron[:location].zip(dendrite).map { |x, y| (y - x) ** 2 }
+		#random x,y,z that is +/-10 from neuron 
+		@neuron[:location].each {|pt| [true,false].sample ? dendrite << pt+rand(10) : dendrite << pt-rand(10)} 
+		#calc sqaures of distance between neuron [x,y,z] and dendrite [j,k,i] points 
+		#zipped array becomes [ [x,j], [y,k], [z,i] ] format
+		dendrite << @neuron[:location].zip(dendrite).map { |x, j| (j - x) ** 2 } 
+		#PYTHAGOREAAAAAAAAAN!!!!!!!
 		dendrite[3][0] = sqrt_to_200(dendrite[3][0],dendrite[3][1])
 		dendrite[3][0] = sqrt_to_200(dendrite[3][0],dendrite[3][2])
 		dendrite[3] = dendrite[3][0]
-
 		# It is computationally faster to build a decision tree for converting 'sum'
 		# to an integer than to find sqrt of 'sum' (see method 'sqrt_to_100')
-		# woopty do
 
-		
-
+		#last attribute of dendrite is it's strength. strength decreases over time. increases with use.
+		dendrite << 10
 		return dendrite
 	end
 end
-
-=begin
-	def establish
-		@@neuron_hash = {@location.join("_").to_sym => [@delay, [@dendrites]]}
-		require 'pry'; binding.pry
-		@@dendrites_hash = {@location.join("_") + }
-	end
-=end
-
